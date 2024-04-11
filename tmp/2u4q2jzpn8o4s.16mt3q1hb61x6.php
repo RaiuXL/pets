4973f1001cf67a2177d6ea4fa85660ad60083cd6
Nathan Tapia-Ramirez <ntapiaramirez@outlook.com>
2024-04-11 13:57:14 -0400

    Updated images, gitignore, styles and accidentally included tmp :(

---
+++ tmp/2u4q2jzpn8o4s.16mt3q1hb61x6.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles/styles.css">
+    <title>Pets Home Page</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Pet Home</h1>
+        <img src="images/gary.jpeg" alt="Just a little snail guy">
+        <a href="https://github.com/RaiuXL/pets">link to my Repo 'Pets' 4/11</a>
+    </div>
+</body>
+</html>
